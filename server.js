6dbf51617bb5003966739bd4a64c7505dc1f1211
oxayaza <gashishmarihuanovich@gmail.com>
2021-04-05 03:12:50 +0300

    Completed recipient page

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-let
+const
   server    = require( 'oxyz-express' ),
-  multipart = require( 'connect-multiparty' );
+  multipart = require( 'connect-multiparty' ),
+  sse       = require( 'server-sent-events' ),
+  Emitter   = require( 'events' );
 
 server({
   pug: {
@@ -10,14 +12,27 @@ server({
     root: 'dev'
   },
   cb: function ( app ) {
-    let multipartMiddleware = multipart();
+    let
+      multipartMiddleware = multipart(),
+      emitter = new Emitter();
 
     // alert request handler
-    app.post( /.+alrreq$/, multipartMiddleware, function ( req, res ) {
+    app.post( '/alrreq', multipartMiddleware, function ( req, res ) {
       console.log( '[alr req]', req.body );
       let response = req.body;
+      emitter.emit( 'alert', response );
       console.log( '[alr res]', response );
       res.send( response );
+    });
+
+    // event sender
+    app.get( '/events', sse, function( req, res ) {
+      console.log( '[sub]', req.headers['x-forwarded-for'] || req.connection.remoteAddress );
+
+      emitter.on( 'alert', ( data ) => {
+        let msg = 'event: alert\ndata: '+ ( JSON.stringify( data ) ) +'\n\n';
+        res.sse( msg );
+      });
     });
   }
 });
